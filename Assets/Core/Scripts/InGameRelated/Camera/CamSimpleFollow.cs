ddcfa8f9b26686294039b09b7838382446cfbc82
DevBorisElkin <ea.foreverrr@gmail.com>
2021-09-04 21:29:13 +0300

    Cool cam features

--- Assets/Core/Scripts/InGameRelated/Camera/CamSimpleFollow.cs
+++ Assets/Core/Scripts/InGameRelated/Camera/CamSimpleFollow.cs
@@ -12,10 +12,16 @@ public class CamSimpleFollow : MonoBehaviour
     Quaternion baseRotation;
 
     [Header("Falling out confition")]
+    public bool useRandomCamAnimOnFall = true;
     bool isFalling;
     public Vector3 fallingOffset;
     public float fallingPositionLerpSpeed = 3f;
     public float fallingRotationLerpSpeed = 1f;
+    [Space(5f)]
+    public float camBehingPlayerOnFall = 5f;
+    public float camAbovePlayerOnFall = 17f;
+
+    public CamFollowOnDeath camFollowOnDeath;
 
     private void Start()
     {
@@ -32,8 +38,64 @@ public class CamSimpleFollow : MonoBehaviour
                 transform.position = Vector3.Lerp(transform.position, transformToFollow.position + offset, lerpSpeed * Time.deltaTime);
             else if (isFalling)
             {
-                transform.position = Vector3.Lerp(transform.position, transformToFollow.position + fallingOffset, fallingPositionLerpSpeed * Time.deltaTime);
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation((transformToFollow.position - transform.position).normalized), fallingRotationLerpSpeed);
+                if (camFollowOnDeath.Equals(CamFollowOnDeath.SimpleFall))
+                {
+                    transform.position = Vector3.Lerp(transform.position, transformToFollow.position + fallingOffset, fallingPositionLerpSpeed * Time.deltaTime);
+                    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation((transformToFollow.position - transform.position).normalized), fallingRotationLerpSpeed);
+                }
+                else if (camFollowOnDeath.Equals(CamFollowOnDeath.RotateWithPlayer))
+                {
+                    transform.position = Vector3.Lerp(transform.position, transformToFollow.position + (-transformToFollow.forward * camBehingPlayerOnFall) + Vector3.up * camAbovePlayerOnFall, fallingPositionLerpSpeed * Time.deltaTime);
+                    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation((transformToFollow.position - transform.position).normalized), fallingRotationLerpSpeed);
+                }
+            }
+        }
+    }
+    public void SetFalling(bool newState)
+    {
+        if (newState && useRandomCamAnimOnFall) camFollowOnDeath = (CamFollowOnDeath)UnityEngine.Random.Range(0, 2);
+        if (!newState) transform.rotation = baseRotation;
+        isFalling = newState;
+    }
+
+
+    public enum CamFollowOnDeath { SimpleFall = 0, RotateWithPlayer = 1}
+
+    /* way 2
+     * 
+     public Vector3 offset;
+    public float lerpSpeed = 3f;
+
+    public Transform transformToFollow;
+
+    Quaternion baseRotation;
+
+    [Header("Falling out confition")]
+    bool isFalling;
+    public Vector3 fallingOffset;
+    public float fallingPositionLerpSpeed = 3f;
+    public float fallingRotationLerpSpeed = 1f;
+    
+
+    private void Start()
+    {
+        baseRotation = transform.rotation;
+        if(FindObjectOfType<PlayerMovementController>() != null)
+            if (transformToFollow == null) transformToFollow = FindObjectOfType<PlayerMovementController>().transform;
+    }
+
+    void FixedUpdate()
+    {
+        if (transformToFollow != null)
+        {
+            if(!isFalling)
+                transform.position = Vector3.Lerp(transform.position, transformToFollow.position + offset, lerpSpeed * Time.deltaTime);
+            else if (isFalling)
+            {
+                Quaternion lookRot = Quaternion.LookRotation((transformToFollow.position - transform.position).normalized);
+                transform.position = Vector3.Lerp(transform.position, transformToFollow.position + (-transformToFollow.forward * camBehingPlayerOnFall) + Vector3.up * camAbovePlayerOnFall, fallingPositionLerpSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.Lerp(transform.rotation, lookRot, fallingRotationLerpSpeed);
+                    
             }
         }
     }
@@ -42,4 +104,5 @@ public class CamSimpleFollow : MonoBehaviour
         isFalling = newState;
         if (!newState) transform.rotation = baseRotation;
     }
+     */
 }
