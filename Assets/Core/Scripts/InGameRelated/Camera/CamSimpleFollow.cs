b943f03231d5cae754e1b9add8df4a400eb4a2cc
DevBorisElkin <ea.foreverrr@gmail.com>
2021-09-23 15:52:59 +0300

    started transition to Cinemachine

--- Assets/Core/Scripts/InGameRelated/Camera/CamSimpleFollow.cs
+++ Assets/Core/Scripts/InGameRelated/Camera/CamSimpleFollow.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CamSimpleFollow : MonoBehaviour
 {
+    CinemachineVirtualCamera cin_cam;
+    CinemachineTransposer cin_transposer;
+    CinemachineComposer cin_composer;
+
     public Vector3 offset;
+    public Vector3 fallingOffset;
+
     public float lerpSpeed = 3f;
 
     public Transform transformToFollow;
@@ -14,7 +21,7 @@ public class CamSimpleFollow : MonoBehaviour
     [Header("Falling out confition")]
     public bool useRandomCamAnimOnFall = true;
     bool isFalling;
-    public Vector3 fallingOffset;
+    
     public float fallingPositionLerpSpeed = 3f;
     public float fallingRotationLerpSpeed = 1f;
     [Space(5f)]
@@ -29,38 +36,88 @@ public class CamSimpleFollow : MonoBehaviour
         baseRotation = transform.rotation;
         if(FindObjectOfType<PlayerMovementController>() != null)
             if (transformToFollow == null) transformToFollow = FindObjectOfType<PlayerMovementController>().transform;
-    }
 
-    void FixedUpdate()
-    {
-        if (transformToFollow != null)
+        cin_cam = FindObjectOfType<CinemachineVirtualCamera>();
+
+        if(transformToFollow != null && cin_cam != null)
         {
-            if(!isFalling)
-                transform.position = Vector3.Lerp(transform.position, transformToFollow.position + offset, lerpSpeed * Time.deltaTime);
-            else if (isFalling)
-            {
-                if (camFollowOnDeath.Equals(CamFollowOnDeath.SimpleFall))
-                {
-                    transform.position = Vector3.Lerp(transform.position, transformToFollow.position + fallingOffset, fallingPositionLerpSpeed * Time.deltaTime);
-                    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation((transformToFollow.position - transform.position).normalized), fallingRotationLerpSpeed);
-                }
-                else if (camFollowOnDeath.Equals(CamFollowOnDeath.RotateWithPlayer))
-                {
-                    transform.position = Vector3.Lerp(transform.position, transformToFollow.position + (-transformToFollow.forward * camBehingPlayerOnFall) + Vector3.up * camAbovePlayerOnFall, fallingPositionLerpSpeed * Time.deltaTime);
-                    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation((transformToFollow.position - transform.position).normalized), fallingRotationLerpSpeed);
-                }
-            }
+            cin_transposer = cin_cam.GetCinemachineComponent<CinemachineTransposer>();
+            cin_composer = cin_cam.GetCinemachineComponent<CinemachineComposer>();
+
+            BasicSetUp();
         }
     }
+
+    //void FixedUpdate()
+    //{
+    //    if (transformToFollow != null)
+    //    {
+    //        if(!isFalling)
+    //            transform.position = Vector3.Lerp(transform.position, transformToFollow.position + offset, lerpSpeed * Time.deltaTime);
+    //        else if (isFalling)
+    //        {
+    //            if (camFollowOnDeath.Equals(CamFollowOnDeath.SimpleFall))
+    //            {
+    //                transform.position = Vector3.Lerp(transform.position, transformToFollow.position + fallingOffset, fallingPositionLerpSpeed * Time.deltaTime);
+    //                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation((transformToFollow.position - transform.position).normalized), fallingRotationLerpSpeed);
+    //            }
+    //            else if (camFollowOnDeath.Equals(CamFollowOnDeath.RotateWithPlayer))
+    //            {
+    //                transform.position = Vector3.Lerp(transform.position, transformToFollow.position + (-transformToFollow.forward * camBehingPlayerOnFall) + Vector3.up * camAbovePlayerOnFall, fallingPositionLerpSpeed * Time.deltaTime);
+    //                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation((transformToFollow.position - transform.position).normalized), fallingRotationLerpSpeed);
+    //            }
+    //        }
+    //    }
+    //}
     public void SetFalling(bool newState)
     {
         if (newState && useRandomCamAnimOnFall) camFollowOnDeath = (CamFollowOnDeath)UnityEngine.Random.Range(0, 2);
         if (!newState) transform.rotation = baseRotation;
         if (nicknamePlayerCanvas != null) nicknamePlayerCanvas.isMainPlayerFalling = newState;
         isFalling = newState;
+
+        if (isFalling)
+        {
+            
+        }
+        else if (!isFalling)
+        {
+            
+            StartCoroutine(InvocableDelayedSetUp());
+        }
+
     }
 
+    IEnumerator InvocableDelayedSetUp()
+    {
+        InstantTransmissionSetUp();
+        yield return new WaitForSeconds(0.5f);
+        BasicSetUp();
+    }
 
+    void InstantTransmissionSetUp()
+    {
+        cin_transposer.m_XDamping = 0;
+        cin_transposer.m_YDamping = 0;
+        cin_transposer.m_ZDamping = 0;
+        cin_composer.m_HorizontalDamping = 0f;
+        cin_composer.m_VerticalDamping = 0f;
+    }
+
+    void BasicSetUp()
+    {
+        cin_transposer.m_XDamping = 1.3f;
+        cin_transposer.m_YDamping = 1.3f;
+        cin_transposer.m_ZDamping = 1.3f;
+        cin_transposer.m_FollowOffset = offset;
+        cin_composer.m_HorizontalDamping = 0.5f;
+        cin_composer.m_VerticalDamping = 0.5f;
+        cin_composer.m_DeadZoneWidth = 0.2f;
+        cin_composer.m_DeadZoneHeight = 0.2f;
+        cin_cam.Follow = transformToFollow;
+        cin_cam.LookAt = transformToFollow;
+        cin_transposer.m_BindingMode = CinemachineTransposer.BindingMode.WorldSpace;
+    }
     public enum CamFollowOnDeath { SimpleFall = 0, RotateWithPlayer = 1}
 
     /* way 2
